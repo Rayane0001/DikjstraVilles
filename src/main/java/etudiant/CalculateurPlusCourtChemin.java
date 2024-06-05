6051380c7bcaece127168b56d31c34a0fdca8640
Rayane0001 <rousseau.rayane@gmail.com>
2024-06-05 10:49:42 +0200

    Ajout du plantuml et du png

--- src/main/java/etudiant/CalculateurPlusCourtChemin.java
+++ src/main/java/etudiant/CalculateurPlusCourtChemin.java
@@ -1,6 +1,8 @@
 package etudiant;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 import jfc.ICalculateurPlusCourtChemin;
 import jfc.Ville;
@@ -13,14 +15,49 @@ public class CalculateurPlusCourtChemin implements ICalculateurPlusCourtChemin{
 
 	@Override
 	public ArrayList<Ville> plusCourtChemin(ArrayList<Ville> toutesLesVilles, Ville villeDepart, Ville villeArrivee) {
-		
-		ArrayList<Ville> chemin=new ArrayList<Ville>();
-		// A modifier
-		// Supprimer les deux lignes suivantes et mettre votre code.
-		chemin.add(villeDepart);
-		chemin.add(villeArrivee);
-		
+
+		ArrayList<Ville> chemin = new ArrayList<>();
+		ArrayList<Ville> villesNonVisitees = new ArrayList<>(toutesLesVilles);
+		ArrayList<Double> cout = new ArrayList<>(Collections.nCopies(toutesLesVilles.size(), Double.POSITIVE_INFINITY));
+		ArrayList<Ville> predecesseur = new ArrayList<>(Collections.nCopies(toutesLesVilles.size(), null));
+
+		cout.set(toutesLesVilles.indexOf(villeDepart), 0.0);
+
+		while (!villesNonVisitees.isEmpty()) {
+			Ville villeActuelle = null;
+			double minCout = Double.POSITIVE_INFINITY;
+
+			for (Ville ville : villesNonVisitees) {
+				double villeCout = cout.get(toutesLesVilles.indexOf(ville));
+				if (villeCout < minCout) {
+					villeActuelle = ville;
+					minCout = villeCout;
+				}
+			}
+
+			if (villeActuelle.equals(villeArrivee)) {
+				break;
+			}
+
+			villesNonVisitees.remove(villeActuelle);
+
+			for (Ville villeVoisine : villeActuelle.getVillesVoisines()) {
+				double coutPotentiel = cout.get(toutesLesVilles.indexOf(villeActuelle)) + villeActuelle.getDistanceMetres(villeVoisine);
+				int indexVoisine = toutesLesVilles.indexOf(villeVoisine);
+
+				if (coutPotentiel < cout.get(indexVoisine)) {
+					cout.set(indexVoisine, coutPotentiel);
+					predecesseur.set(indexVoisine, villeActuelle);
+				}
+			}
+		}
+
+		Ville villeActuelle = villeArrivee;
+		while (villeActuelle != null) {
+			chemin.add(0, villeActuelle);
+			villeActuelle = predecesseur.get(toutesLesVilles.indexOf(villeActuelle));
+		}
+
 		return chemin;
 	}
-
 }
